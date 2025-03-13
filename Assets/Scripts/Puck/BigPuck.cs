951cc54a8f3d422d31c43eddd20b0ae6c93e96cc
Merlin4854 <152187653+Merlin4854@users.noreply.github.com>
2025-03-13 17:40:21 +0100

    Update BigPuck.cs

--- Assets/Scripts/Puck/BigPuck.cs
+++ Assets/Scripts/Puck/BigPuck.cs
@@ -4,8 +4,6 @@ public class BigPuck : Puck
 {
     protected override void ApplyPuckSettings()
     {
-        base.ApplyPuckSettings();
-        transform.localScale *= 1.5f; // Increases size by 50%
-        Debug.Log("BigPuck: Scale increased.");
+
     }
 }
